c345117c6bfbeb8371cc6cb414b52330bbf025dc
prasantnayak <prasant.nayak@gmail.com>
2016-09-22 21:10:16 +0530

    Updated

--- mod1_solutions/app.js
+++ mod1_solutions/app.js
@@ -9,20 +9,22 @@
 
     $scope.menuCheck = function (){
       var menu_items= $scope.lunch_menu.split(',');
-      var menucount = menu_items.length-1;
+      var menucount = menu_items.length;
       console.log(menu_items);
-            console.log(menucount);
-          if (menucount ==0)
-          {
-            $scope.messageDisp= "Please enter data first"
-          }
-         else if (menucount <=3)
+      console.log(menucount);
+     if(menu_items == "")
+     {
+       $scope.messageDisp= "Please enter data first";
+     }
+     else {
+      if (menucount <=3)
           {
             $scope.messageDisp= "Enjoy!";
           }
           else if (menucount>3) {
             $scope.messageDisp="Too Much";
           }
+        }
   };
 }
 })();
